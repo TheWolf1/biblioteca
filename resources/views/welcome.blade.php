c1dd040e997c2456d71212d04bdcb63a3b4f18cd
TheWolf1 <pazortizkevindaniel@gmail.com>
2020-04-11 15:26:11 -0500

    se le cambio el nombre a mi pagina ya no es laravel ahora es laravel kevin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel kevin
                 </div>
 
                 <div class="links">
